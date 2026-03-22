b9cdbdf70226d3872debef2f986a23eb8190bcd3
VladRotaru1 <vlad.rotaru@student.usv.ro>
2026-03-22 20:52:41 +0200

    tema lab3

--- ManagerDate.cs
+++ ManagerDate.cs
@@ -25,5 +25,46 @@ namespace TargMasini
             tranzactii.Add(t);
             Console.WriteLine("Tranzactie inregistrata cu succes.");
         }
+        public void AfiseazaTranzactii()
+        {
+            Console.WriteLine("Tranzactii inregistrate:");
+            foreach (var t in tranzactii)
+            {
+                Console.WriteLine($"Vanzator: {t.NumeVanzator}, Cumparator: {t.NumeCumparator}, Masina: {t.MasinaVanduta.Firma} {t.MasinaVanduta.Model}, Pret: {t.Pret}, Data: {t.DataTranzactie}");
+            }
+        }
+        public void AfiseazaTranzactiiDupaData(DateTime data)
+        {
+            Console.WriteLine($"Tranzactii inregistrate pentru data: {data.ToShortDateString()}");
+            foreach (var t in tranzactii)
+            {
+                if (t.DataTranzactie.Date == data.Date)
+                {
+                    Console.WriteLine($"Vanzator: {t.NumeVanzator}, Cumparator: {t.NumeCumparator}, Masina: {t.MasinaVanduta.Firma} {t.MasinaVanduta.Model}, Pret: {t.Pret}");
+                }
+            }
+        }
+        public void AfiseazaTranzactiiDupaFirma(string firma)
+        {
+            Console.WriteLine($"Tranzactii inregistrate pentru firma: {firma}");
+            foreach (var t in tranzactii)
+            {
+                if (t.MasinaVanduta.Firma.Equals(firma, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Vanzator: {t.NumeVanzator}, Cumparator: {t.NumeCumparator}, Masina: {t.MasinaVanduta.Firma} {t.MasinaVanduta.Model}, Pret: {t.Pret}, Data: {t.DataTranzactie}");
+                }
+            }
+        }
+        public void AfiseazaTranzactiiDupaPret(decimal pretMinim, decimal pretMaxim)
+        {
+            Console.WriteLine($"Tranzactii inregistrate pentru pret intre {pretMinim} si {pretMaxim}");
+            foreach (var t in tranzactii)
+            {
+                if (t.Pret >= pretMinim && t.Pret <= pretMaxim)
+                {
+                    Console.WriteLine($"Vanzator: {t.NumeVanzator}, Cumparator: {t.NumeCumparator}, Masina: {t.MasinaVanduta.Firma} {t.MasinaVanduta.Model}, Pret: {t.Pret}, Data: {t.DataTranzactie}");
+                }
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
 using TargMasini;
 
 // 1. Initializam clasa de gestiune
@@ -6,12 +8,22 @@ ManagerDate manager = new ManagerDate();
 
 // 2. Cream o masina noua (folosind clasa creata anterior)
 Masina masina1 = new Masina("Opel", "Astra 1.4", 2015, "Gri", "AC, ABS");
-
+Masina masina2 = new Masina("VW", "Polo", 2019, "ALB", "Standard");
 // 3. Cream o tranzactie
 Tranzactie t1 = new Tranzactie("Popescu Ion", "Ionescu Maria", masina1, 5500, DateTime.Now);
-
+Tranzactie t2 = new Tranzactie("Vasilica Maria", "Ion Marius", masina2, 7000, DateTime.Now);
 // 4. Adaugam tranzactia (aici se va verifica si avertizarea)
 manager.AdaugaTranzactie(t1);
+manager.AdaugaTranzactie(t2);
+
+manager.AfiseazaTranzactii();
+
+Console.Write("\nIntroduceti firma cautata: ");
+string firma = Console.ReadLine();
+
+manager.AfiseazaTranzactiiDupaFirma(firma);
+manager.AfiseazaTranzactiiDupaData(DateTime.Now);
+manager.AfiseazaTranzactiiDupaPret(5000, 6000);
 
 // Tinem consola deschisa ca sa vedem rezultatul
 Console.WriteLine("Apasa orice tasta pentru a inchide...");
